68a4f421c98dc4919e3d0b591c12abec99d51c60
amir-dell <amir.h.kadivar@gmail.com>
2013-07-22 12:10:50 -0400

    remove unnecessary files from former branch, update metadata

--- recipes/default.rb
+++
@@ -1,9 +0,0 @@
-## Cookbook Name:: deploy-drupal
-## Recipe:: default
-##
-
-include_recipe 'deploy-drupal::dependencies'
-include_recipe 'deploy-drupal::get_project'
-include_recipe 'deploy-drupal::download_drupal'
-include_recipe 'deploy-drupal::prepare'
-include_recipe 'deploy-drupal::install' 
--- recipes/drupal_solr.rb
+++ recipes/drupal_solr.rb
@@ -4,12 +4,9 @@
 
 include_recipe 'deploy-drupal::install_solr'
 
-DEPLOY_SITE_DIR         = node['deploy-drupal']['deploy_dir']  + "/" +
-                          node['deploy-drupal']['project_name'] + "/" +
-                          node['deploy-drupal']['drupal_root_dir']
-
+# i need: site_dir, db_name for mysql_connection
 SOLR_PHP_CLIENT_DIR     = DEPLOY_SITE_DIR + "/" +
-                          node['deploy-drupal']['solr']['apachesolr_install_dir'] +
+                          node['drupal-solr']['apachesolr_install_dir'] +
                           "/apachesolr"
 DRUSH                   = "drush --root='#{DEPLOY_SITE_DIR}'"
 
@@ -22,15 +19,15 @@ DB_ROOT_CONNECTION      = [ "mysql",
 
 UPDATE_SOLR_SERVER_SQL  = [ "UPDATE apachesolr_environment",
                             "SET url=",
-                            "'localhost:#{node['tomcat']['port']}/#{node['deploy-drupal']['solr']['app_name']}'",
+                            "'localhost:#{node['tomcat']['port']}/#{node['drupal-solr']['app_name']}'",
                             "WHERE env_id = 'solr'"
                           ].join(' ')
 
 DRUPAL_SOLR_CONF_DIR    = DEPLOY_SITE_DIR +
-                          node['deploy-drupal']['solr']['apachesolr_conf_dir']
+                          node['drupal-solr']['apachesolr_conf_dir']
 
 execute "download-apachesolr-module" do
-  command "#{DRUSH} dl apachesolr -y --destination=#{node['deploy-drupal']['solr']['apachesolr_install_dir']}"
+  command "#{DRUSH} dl apachesolr -y --destination=#{node['drupal-solr']['apachesolr_install_dir']}"
   not_if "#{DRUSH} pm-list | grep apachesolr"
   notifies :run, "bash[install-apachesolr-module]", :immediately
   notifies :run, "execute[drush-cache-clear]", :delayed
@@ -38,9 +35,9 @@ execute "download-apachesolr-module" do
 end
 
 bash "install-apachesolr-module" do
-  cwd DEPLOY_SITE_DIR + "/" + node['deploy-drupal']['solr']['apachesolr_install_dir']
+  cwd DEPLOY_SITE_DIR + "/" + node['drupal-solr']['apachesolr_install_dir']
   code <<-EOH
-    curl #{node['deploy-drupal']['solr']['php_client_url']} | tar xz
+    curl #{node['drupal-solr']['php_client_url']} | tar xz
     #{DRUSH} en apachesolr apachesolr_search apachesolr_access -y
   EOH
   action :nothing
@@ -48,12 +45,12 @@ bash "install-apachesolr-module" do
 end
 
 bash "drupalize-solr-conf-files" do
-  cwd node['deploy-drupal']['solr']['home_dir']
+  cwd node['drupal-solr']['home_dir']
   code <<-EOH
     solr_config_files=( "protwords.txt" "schema.xml" "solrconfig.xml" )
     for file in "${solr_config_files[@]}"; do
       cp conf/$file conf/$file.bak ;
-      cp #{DEPLOY_SITE_DIR}/#{node['deploy-drupal']['solr']['apachesolr_conf_dir']}/$file conf/$file ;
+      cp #{DEPLOY_SITE_DIR}/#{node['drupal-solr']['apachesolr_conf_dir']}/$file conf/$file ;
       chown -R #{node['tomcat']['user']}:#{node['tomcat']['group']} .
     done
   EOH
@@ -71,7 +68,7 @@ end
 
 execute "set-solr-as-default-search" do
   command "#{DRUSH} vset search_default_module apachesolr_search"
-  only_if { node['deploy-drupal']['solr']['make_solr_default_search'] == "true" }
+  only_if { node['drupal-solr']['make_solr_default_search'] == "true" }
 end
 
 # to test wether drush works:
