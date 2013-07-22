782bf93fba4b4e83fed21483fd146a5a312aef26
amir-dell <amir.h.kadivar@gmail.com>
2013-07-22 13:30:18 -0400

    fix filenames and cleanup deploy-drupal remainders

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,48 +1,88 @@
-## Cookbook Name:: deploy-drupal
-## Recipe:: install_solr
+## Cookbook Name:: drupal-solr
+## Recipe:: default
 ##
 
-%w{curl drush tomcat}.each { |recipe| include_recipe recipe }
+include_recipe "drupal-solr::install_solr"
+include_recipe "drush"
 
-SOLR_CONTEXT_FILE       = node['tomcat']['context_dir'] + "/" +
-                          node['drupal-solr']['app_name'] + ".xml"
+SOLR_PHP_CLIENT_DIR     = node['drupal-solr']['drupal_root'] + "/" +
+                          node['drupal-solr']['apachesolr_install_dir'] +
+                          "/apachesolr"
+DRUSH                   = "drush --root='#{node['drupal-solr']['drupal_root']}'"
 
-SOLR_ARCHIVE            = "apache-solr-" + node['drupal-solr']['version']
+DB_ROOT_CONNECTION      = [ "mysql",
+                            "--user='root'",
+                            "--host='localhost'",
+                            "--password='#{node['mysql']['server_root_password']}'",
+                            "--database='#{node['drupal-solr']['drupal_db']}'"
+                          ].join(' ')
 
+UPDATE_SOLR_SERVER_SQL  = [ "UPDATE apachesolr_environment",
+                            "SET url=",
+                            "'localhost:#{node['tomcat']['port']}/#{node['drupal-solr']['app_name']}'",
+                            "WHERE env_id = 'solr'"
+                          ].join(' ')
 
-# directory containing solr.war file
-# if war file is removed tomcat undeploys application
-directory node['drupal-solr']['root_dir'] do
-  owner node['tomcat']['user']
-  group node['tomcat']['group']
-  mode 0775
-  recursive true
+DRUPAL_SOLR_CONF_DIR    = node['drupal-solr']['drupal_root'] +
+                          node['drupal-solr']['apachesolr_conf_dir']
+
+execute "download-apachesolr-module" do
+  command "#{DRUSH} dl apachesolr -y --destination=#{node['drupal-solr']['apachesolr_install_dir']}"
+  not_if "#{DRUSH} pm-list | grep apachesolr"
+  notifies :run, "bash[install-apachesolr-module]", :immediately
+  notifies :run, "execute[drush-cache-clear]", :delayed
+  notifies :run, "execute[drush-cron]", :delayed
 end
 
-# solr/home directory
-directory node['drupal-solr']['home_dir'] do
-  owner node['tomcat']['user']
-  group node['tomcat']['group']
-  mode 0775
-  recursive true
-end
-
-bash "download-solr-#{node['drupal-solr']['version']}" do
-  user node['tomcat']['user']
-  cwd "#{node['drupal-solr']['home_dir']}/.."
+bash "install-apachesolr-module" do
+  cwd node['drupal-solr']['drupal_root'] + "/" + node['drupal-solr']['apachesolr_install_dir']
   code <<-EOH
-    curl #{node['drupal-solr']['url']} | tar xz
-    cp #{SOLR_ARCHIVE}/example/webapps/solr.war .
-    cp -Rf #{SOLR_ARCHIVE}/example/solr/. #{node['drupal-solr']['home_dir']}/
+    curl #{node['drupal-solr']['php_client_url']} | tar xz
+    #{DRUSH} en apachesolr apachesolr_search apachesolr_access -y
   EOH
-  creates node['drupal-solr']['home_dir'] + "/conf/schema.xml"
-  notifies :restart, "service[tomcat]", :delayed
+  action :nothing
+  notifies :run, "bash[drupalize-solr-conf-files]", :immediately
 end
 
-template SOLR_CONTEXT_FILE do
-  owner node['tomcat']['user']
-  group node['tomcat']['group']
-  mode 0644
-  source "solr_context.xml.erb"
-  notifies :restart, "service[tomcat]"
+bash "drupalize-solr-conf-files" do
+  cwd node['drupal-solr']['home_dir']
+  code <<-EOH
+    solr_config_files=( "protwords.txt" "schema.xml" "solrconfig.xml" )
+    for file in "${solr_config_files[@]}"; do
+      cp conf/$file conf/$file.bak ;
+      cp #{node['drupal-solr']['drupal_root']}/#{node['drupal-solr']['apachesolr_conf_dir']}/$file conf/$file ;
+      chown -R #{node['tomcat']['user']}:#{node['tomcat']['group']} .
+    done
+  EOH
+  action :nothing
+  notifies :run, "execute[connect-drupal-solr]", :immediately
+end
+
+# update the apachesolr_environment table to contain
+# url of the installed solr server
+execute "connect-drupal-solr" do
+  command "#{DB_ROOT_CONNECTION} -e \"#{UPDATE_SOLR_SERVER_SQL}\""
+  action :nothing
+  notifies :run, "execute[set-solr-as-default-search]", :immediately
+end
+
+execute "set-solr-as-default-search" do
+  command "#{DRUSH} vset search_default_module apachesolr_search"
+  only_if { node['drupal-solr']['make_solr_default_search'] }
+end
+
+# to test wether drush works:
+# mysql -u root -proot --database=drupal -e "truncate table search_index" and then
+# use search and see if it work
+
+# drush cache clear
+execute "drush-cache-clear" do
+  command "#{DRUSH} cache-clear all"
+  action :nothing
+end
+
+# run cron to index content for newly created solr search server
+execute "drush-cron" do
+  command "#{DRUSH} cron"
+  action :nothing
 end
