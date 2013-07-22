7675dc41d7a46ee5a599ef3cdb5f47a3b045d121
amir-dell <amir.h.kadivar@gmail.com>
2013-07-22 13:14:56 -0400

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,6 @@ description      "Installs/Configures Solr and Connects to a Drupal site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends "apt"
-depends "build-essential"
-depends "git"
-depends "vim"
 depends "curl"
-depends "php"
 depends "drush"
-depends "mysql"
 depends "tomcat"
